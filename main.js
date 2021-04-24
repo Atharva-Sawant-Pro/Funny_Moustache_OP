b4ac23d68d7a0961c1483144f9acc327d57527b8
Atharva-Sawant-Pro <78305129+Atharva-Sawant-Pro@users.noreply.github.com>
2021-04-24 10:51:56 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+noseX=0;
+noseY=0;
 function preload(){
 
 }
@@ -5,7 +7,21 @@ function setup(){
     canvas=createCanvas(300,300);
     canvas.center()
     video=createCapture(VIDEO);
+    video.size(300,300);
     video.hide();
+    poseNet=ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose',gotPoses);
+}
+function gotPoses(results){
+    if (results.length>0){
+        console.log(results);
+        console.log("noseX="+results[0].pose.nose.x);
+        console.log("noseY="+results[0].pose.nose.y);
+    }
+    
+}
+function modelLoaded(){
+    console.log("pooos nat is inisolized");
 }
 function draw(){
 image(video,0,0,300,300)
